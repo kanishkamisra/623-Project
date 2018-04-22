c54801376e03118c7886c4198a39b2554a84b2c5
kanishkamisra <menogetusername@gmail.com>
2018-04-21 21:39:58 -0400

    more pointless code

--- utils.R
+++ utils.R
@@ -15,19 +15,29 @@ col_name <- function(x, default = stop("Please supply column name", call. = FALS
   stop("Invalid column specification", call. = FALSE)
 }
 
-pairwise_temp <- function(tbl, item, feature, value,
-                          method = "euclidean", ...) {
-  pairwise_dist_(tbl,
-                 col_name(substitute(item)),
-                 col_name(substitute(feature)),
-                 col_name(substitute(value)),
-                 method = method, ...)
+multi_scale <- function(tbl, item1, item2, value, k = 2) {
+  multi_scale_(tbl,
+               col_name(substitute(item1)),
+               col_name(substitute(item2)),
+               col_name(substitute(value)),
+               k = 2)
 }
 
-pairwise_temp_ <- function(tbl, item, feature, value) {
-  d_func <- squarely_(function(m) ))
+
+multi_scale_ <- function(tbl, item1, item2, value, k = 2) {
+  tbl_matrix <- tbl %>%
+    spread(item2, col_name(value), fill = 0) %>%
+    as.data.frame() %>%
+    remove_rownames() %>%
+    column_to_rownames("item1") %>%
+    as.matrix()
   
-  tbl %>%
-    d_func(item, feature, value) %>%
-    rename(distance = value)
+  cmdscale(tbl_matrix, k = k) %>%
+    as.data.frame() %>%
+    rownames_to_column("item") %>%
+    as.tibble()
+}
+
+add_rowid <- function(x) {
+  x %>% mutate(article_id = row_number())
 }
